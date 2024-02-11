2d3520a7c9b8b7ddb1d497df7e55807bf464b94e
Carl Cat <nikimouse100@gmail.com>
2024-02-11 23:07:58 +0300

    Прозрачный фон разделителя '...' в пагинации

--- resources/views/components/pagination.blade.php
+++ resources/views/components/pagination.blade.php
@@ -16,7 +16,7 @@
             @foreach ($elements as $element)
                 {{-- "Three Dots" Separator --}}
                 @if (is_string($element))
-                    <li class="disabled" aria-disabled="true"><span>{{ $element }}</span></li>
+                    <li class="pagination-dots" aria-disabled="true"><span>{{ $element }}</span></li>
                 @endif
 
                 {{-- Array Of Links --}}
